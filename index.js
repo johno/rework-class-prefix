fcaa93a89e9829c9fd37738f3045ab6ca4183ee2
John Otander <johnotander@gmail.com>
2015-01-04 20:52:50 -0700

    Add ignored selector tests

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
-module.exports = function classPrefix(prefix) {
+module.exports = function classPrefix(prefix, options) {
+  options = options || {};
+
   return function classPrefix(styling) {
     var walk = require('rework-walk');
     walk(styling, function(rule, node) {
       if (!rule.selectors) return rule;
 
       rule.selectors = rule.selectors.map(function(selector) {
+        
         if (selector.indexOf('.') === 0) {
           return selector.split('.').join('.' + prefix);
         } else {
--- test/test.js
+++ test/test.js
@@ -8,9 +8,17 @@ function fixture(name) {
 }
 
 describe('rework-class-prefix', function() {
+
   it('prefixes all classes', function() {
     var output = rework(fixture('source.css')).use(classPrfx('prfx-')).toString().trim();
     var expected = fixture('source.css.expected');
+
+    assert.equal(output, expected);
+  });
+
+  it('correctly ignores selectors when specified', function() {
+    var output = rework(fixture('ignored-selectors.css')).use(classPrfx('prfx-', { ignored: ['.ignored-class', /prefix/] })).toString().trim();
+    var expected = fixture('ignored-selectors.css.expected');
 
     assert.equal(output, expected);
   });
